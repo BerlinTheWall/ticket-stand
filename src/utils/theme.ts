0b91382f4976f2dfaceac0ca4e9c535bd0df3d39
Hooman Shahidi <hooman.ksh@gmail.com>
2023-08-11 19:52:29 +0330

    chore: update mui theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,30 +1,40 @@
-import { Roboto } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
 
 export const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
 });
 
 // Create a theme instance.
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#556cd6',
-        },
-        secondary: {
-            main: '#19857b',
-        },
-        error: {
-            main: red.A400,
-        },
+let theme = createTheme({
+  palette: {
+    background: {
+      default: "#0D0C0F",
     },
-    typography: {
-        fontFamily: roboto.style.fontFamily,
+    primary: {
+      main: "#00925D",
     },
+    secondary: {
+      main: "#EB3F5E",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
 });
 
+// theme = createTheme(theme, {
+//   palette: {
+//     background: {
+//         default: theme.palette.common.black
+//     }
+//   },
+// });
 export default theme;
