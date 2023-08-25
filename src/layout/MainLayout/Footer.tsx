63e14c262d6d1bbadbd8450f788fd0333813a93a
Hooman Shahidi <hooman.ksh@gmail.com>
2023-08-25 19:43:50 +0330

    refactor: footer component

--- src/layout/MainLayout/Footer.tsx
+++ src/layout/MainLayout/Footer.tsx
@@ -1,15 +1,90 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 import { common } from "@mui/material/colors";
 import Link from "next/link";
 
+const FOOTER_LINKS = [
+  { title: "Home", link: "/" },
+  { title: "Discover", link: "/" },
+  { title: "Influence", link: "/" },
+  { title: "Release", link: "/" },
+];
+
+const FOOTER_LINKS_2 = [
+  { title: "Privacy policy", link: "/" },
+  { title: "Terms of sevice", link: "/" },
+  { title: "Language", link: "/" },
+];
+
 const Footer: React.FC = () => {
   return (
-    <Box
-      sx={{
-        paddingX: 5,
-      }}
-    >
-      <Box
+    <Container maxWidth="xl">
+      <Grid container spacing={3}>
+        <Grid item lg={2.5} md={6} xs={12}>
+          <Typography fontSize={28}>
+            Our platform is trusted by millions & features best updated movies
+            all around the world.
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          md={true}
+          xs={12}
+          justifyContent={{ md: "end" }}
+          display={"flex"}
+        >
+          <Box display={"grid"} gap={3}>
+            <Stack
+              flexWrap={"wrap"}
+              order={{ md: 1, xs: 2 }}
+              direction={"row"}
+              justifyContent={{ md: "end", sm: "center", xs: "space-between" }}
+            >
+              {FOOTER_LINKS.map((link, index) => {
+                return (
+                  <>
+                    <Link href={link.link} key={link.link}>
+                      {link.title}
+                    </Link>
+                    {index !== FOOTER_LINKS.length - 1 && (
+                      <span style={{ padding: "0px 10px" }}>/</span>
+                    )}
+                  </>
+                );
+              })}
+            </Stack>
+            <Box
+              order={{ md: 2, xs: 1 }}
+              display={"flex"}
+              justifyContent={{ md: "end", xs: "start" }}
+              alignSelf={"end"}
+            >
+              Icons
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+      <Stack
+        direction={{ sm: "row", xs: "column" }}
+        justifyContent={"space-between"}
+        marginTop={3}
+        spacing={3}
+      >
+        <Stack direction={"row"} spacing={3}>
+          {FOOTER_LINKS_2.map((link) => {
+            return (
+              <>
+                <Link prefetch={false} href={link.link} key={link.link}>
+                  <Typography fontSize={{ xs: "13px" }}>
+                    {link.title}
+                  </Typography>
+                </Link>
+              </>
+            );
+          })}
+        </Stack>
+        <Typography textAlign={{ xs: "center" }}>©2023</Typography>
+      </Stack>
+      {/* <Box
         sx={{
           display: "flex",
           flexDirection: { xs: "column", md: "row" },
@@ -31,33 +106,13 @@ const Footer: React.FC = () => {
           }}
         >
           <Box>
-            <Link
-              style={{ textDecoration: "none", color: common.white }}
-              href={"/"}
-            >
-              Home
-            </Link>
+            <Link href={"/"}>Home</Link>
             <span style={{ padding: "0px 10px" }}>/</span>
-            <Link
-              style={{ textDecoration: "none", color: common.white }}
-              href={"/"}
-            >
-              Discover
-            </Link>
+            <Link href={"/"}>Discover</Link>
             <span style={{ padding: "0px 10px" }}>/</span>
-            <Link
-              style={{ textDecoration: "none", color: common.white }}
-              href={"/"}
-            >
-              Influence
-            </Link>
+            <Link href={"/"}>Influence</Link>
             <span style={{ padding: "0px 10px" }}>/</span>
-            <Link
-              style={{ textDecoration: "none", color: common.white }}
-              href={"/"}
-            >
-              Release
-            </Link>
+            <Link href={"/"}>Release</Link>
           </Box>
           <Box>Icons</Box>
         </Box>
@@ -76,8 +131,8 @@ const Footer: React.FC = () => {
           <Typography>Language</Typography>
         </Box>
         <Box>@2023</Box>
-      </Box>
-    </Box>
+      </Box> */}
+    </Container>
   );
 };
 
