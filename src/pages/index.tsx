fd5c87776582ba0c1ff195b910ae5f432d9f9ab7
Hooman Shahidi <hooman.ksh@gmail.com>
2023-09-20 21:58:36 +0330

    chore: add movie api test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
@@ -7,8 +8,24 @@ import ProTip from "../ProTip";
 import Copyright from "../Copyright";
 import MainLayout from "@/layout/MainLayout";
 import SwiperBanner from "@/components/SwiperBanner";
+import axios from "axios";
+import { axiosApi, simpleAxiosApi } from "@/api/newApi";
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_API_URL);
+  const getMovies = async () => {
+    try {
+      const res = await simpleAxiosApi({
+        url: "/movie/popular?language=en-US&page=1",
+      });
+      console.log(res.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <Box
       sx={{
