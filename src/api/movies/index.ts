5adb47f22d9973c6bd9f0509320a7b05017f66fc
Hooman Shahidi <hooman.ksh@gmail.com>
2023-09-27 19:41:48 +0330

    fix: popular movies api

--- src/api/movies/index.ts
+++ src/api/movies/index.ts
@@ -1,14 +1,16 @@
+import { Movie } from "@/types/movie";
 import { simpleAxiosApi } from "../newApi";
+import { AxiosResponse } from "axios";
+import { filteringMethod } from "@/utils/utils";
 
-export const getPopularMovies = async () => {
+export const getPopularMovies = async (
+  filters = {}
+): Promise<AxiosResponse<Movie>> => {
+  const queryParams = filteringMethod(filters);
   try {
     const res = await simpleAxiosApi({
-      url: "/movie/popular",
-      headers: {
-        "Cache-Control": "public, max-age=3600, stale-while-revalidate=59", // 1 hour
-      },
+      url: `/movie/popular${queryParams}`,
     });
-    console.log(res.data.results);
     return res.data.results;
   } catch (error) {
     throw error;
