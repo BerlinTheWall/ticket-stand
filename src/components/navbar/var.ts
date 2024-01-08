153a24c2fae178672eb7e3df0394dd03b7917e3c
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-08 23:08:00 +0330

    refactor: sign out process

--- src/components/navbar/var.ts
+++ src/components/navbar/var.ts
@@ -1,6 +1,6 @@
 import { signOut } from "@/api/login";
-import { SESSION_ID_COOKIE } from "@/constants/cookie";
-import { LOGIN_PAGE } from "@/constants/urls";
+import { ACCOUNT_COOKIE, SESSION_ID_COOKIE } from "@/constants/cookie";
+import { LOGIN_PAGE, PROFILE_PAGE } from "@/constants/urls";
 import Cookies from "js-cookie";
 
 export const NAVBAR_HEIGHT = 85;
@@ -10,16 +10,24 @@ export const PAGE_NOT_MARGIN_NAVBAR = ["/"];
 
 export const PAGES = ["Home", "Discover", "Movie Release"];
 export const PROFILE_ITEMS = [
-  { title: "Profile", icon: "", isLink: true, href: LOGIN_PAGE },
+  { title: "Profile", icon: "", isLink: true, href: PROFILE_PAGE },
   { title: "Account", icon: "", isLink: true, href: LOGIN_PAGE },
   { title: "Dashboard", icon: "", isLink: true, href: LOGIN_PAGE },
   {
     title: "Logout",
     icon: "",
     isLink: false,
-    onClick: () => {
-      const session_id = Cookies.get(SESSION_ID_COOKIE);
-      signOut(session_id!!);
+    onClick: async () => {
+      try {
+        await signOut();
+        console.log("Sign out");
+        Cookies.remove(SESSION_ID_COOKIE);
+        Cookies.remove(ACCOUNT_COOKIE);
+
+        // setUser(null);
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 ];
