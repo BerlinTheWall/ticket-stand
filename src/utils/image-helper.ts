83c9ac9b8e7a67e9ebeae6097f57f0d36d52e0c3
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-13 12:52:02 +0330

    feat: add list background images

--- src/utils/image-helper.ts
+++ src/utils/image-helper.ts
@@ -4,6 +4,20 @@ import LogoBlack from "../../public/assets/images/ticket-stand-high-resolution-l
 import LogoWhite from "../../public/assets/images/ticket-stand-high-resolution-logo-white-on-transparent-background.svg";
 // 404
 import Shrek404 from "../../public/assets/images/shrek-404.png";
+// list background images
+import Background1 from "../../public/assets/images/lists/16.png";
+import Background2 from "../../public/assets/images/lists/05.png";
+import Background3 from "../../public/assets/images/lists/12.png";
+import Background4 from "../../public/assets/images/lists/18.png";
+import Background5 from "../../public/assets/images/lists/3.png";
+import Background6 from "../../public/assets/images/lists/22.png";
+import Background7 from "../../public/assets/images/lists/21.png";
+import Background8 from "../../public/assets/images/lists/19.png";
+import Background9 from "../../public/assets/images/lists/15.png";
+import Background10 from "../../public/assets/images/lists/09.png";
+import Background11 from "../../public/assets/images/lists/07.png";
+import Background12 from "../../public/assets/images/lists/27.png";
+
 // Demo
 import DemoImage from "../../public/assets/images/demo-image.jpg";
 const Images = {
@@ -13,6 +27,19 @@ const Images = {
   DemoImage,
   // 404
   Shrek404,
+  // list background images
+  Background1,
+  Background2,
+  Background3,
+  Background4,
+  Background5,
+  Background6,
+  Background7,
+  Background8,
+  Background9,
+  Background10,
+  Background11,
+  Background12,
 };
 
 export default Images;
