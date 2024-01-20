637cec0d04a16668ab536db3e452579908228916
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-20 16:36:18 +0330

    refactor: get lists hook

--- src/api/profile/hooks/useLists.ts
+++ src/api/profile/hooks/useLists.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useLists = (accountId: string, options = {}) => {
   const lists = useQuery({
-    queryKey: ["lists"],
+    queryKey: ["lists", accountId],
     queryFn: () =>
       axiosApi({ url: `/account/${accountId}/lists` }).then((res) => res.data),
     keepPreviousData: true,
