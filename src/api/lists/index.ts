024045c7727315235e6526895f5ce4f11c4c49b8
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-29 00:48:04 +0330

    feat: list APIs

--- src/api/lists/index.ts
+++ src/api/lists/index.ts
@@ -1,13 +1,30 @@
 import { AxiosResponse } from "axios";
 import { simpleAxiosApi } from "../new-api";
 import { SingleListType } from "@/types/list";
+import { filteringMethod } from "@/utils/utils";
 
-export const getSingleList = async (
-  listId: string
+export const getLists = async (
+  accountId: number,
+  page?: number
 ): Promise<AxiosResponse<SingleListType>> => {
   try {
     const res = await simpleAxiosApi({
-      url: `list/${listId}`,
+      url: `/account/${accountId}/lists`,
+      method: "GET",
+    });
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getSingleList = async (
+  listId: string,
+  page: number
+): Promise<AxiosResponse<SingleListType>> => {
+  try {
+    const res = await simpleAxiosApi({
+      url: `list/${listId}?page=${page}`,
       method: "GET",
     });
     return res;
@@ -28,6 +45,43 @@ export const createList = async (
         name: name,
         description: description,
         language: "en",
+      },
+    });
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const addMovieToList = async (
+  listId: number,
+  mediaId: string
+): Promise<AxiosResponse<any>> => {
+  try {
+    const res = await simpleAxiosApi({
+      url: `list/${listId}/add_item`,
+      method: "POST",
+      data: {
+        media_id: mediaId,
+      },
+    });
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const addTVSerieToList = async (
+  listId: number,
+  mediaId: string
+): Promise<AxiosResponse<any>> => {
+  try {
+    const res = await simpleAxiosApi({
+      url: `list/${listId}/items`,
+      method: "POST",
+      data: {
+        media_id: mediaId,
+        media_type: "tv",
       },
     });
     return res;
