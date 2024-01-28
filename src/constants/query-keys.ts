39ac75b5ef67c7e4674852e1fc7623e997b0522d
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-28 08:58:55 +0330

    feat: add query keys constants

---
+++ src/constants/query-keys.ts
@@ -0,0 +1,19 @@
+const list = "list";
+const treeSpecies = "tree-species";
+const donations = "donations";
+const gallery = "gallery";
+const media = "media";
+const editMedia = "edit-media";
+const blogs = "blogs";
+
+const queryKeys = {
+  list,
+  treeSpecies,
+  donations,
+  gallery,
+  media,
+  editMedia,
+  blogs,
+};
+
+export default queryKeys;
