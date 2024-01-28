5ba1470651859d5c228fbb4e988b81631ba1270c
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-29 00:49:18 +0330

    feat: add single list url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -4,3 +4,4 @@ export const MOVIES_PAGE = "/movies";
 export const SINGLE_MOVIE_PAGE = "/movie";
 export const SINGLE_TVSERIES_PAGE = "/tv-series";
 export const PROFILE_PAGE = "/profile";
+export const SINGLE_LIST_PAGE = "/list";
