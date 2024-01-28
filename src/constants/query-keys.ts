b7f2512781c45b7c6e158b3ede346c3ed4335c19
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-29 00:49:33 +0330

    refactor: query keys

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,19 +1,9 @@
 const list = "list";
-const treeSpecies = "tree-species";
-const donations = "donations";
-const gallery = "gallery";
-const media = "media";
-const editMedia = "edit-media";
-const blogs = "blogs";
+const tvSeries = "episodes";
 
 const queryKeys = {
   list,
-  treeSpecies,
-  donations,
-  gallery,
-  media,
-  editMedia,
-  blogs,
+  tvSeries,
 };
 
 export default queryKeys;
