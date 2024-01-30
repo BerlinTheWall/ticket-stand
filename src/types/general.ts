5cd112a62b6505dfbdb8062453043f90f7ab9711
Hooman Shahidi <hooman.ksh@gmail.com>
2024-01-31 00:38:01 +0330

    feat: add context type

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,1 +1,14 @@
+import { Theme } from "@mui/material";
+import { Profile } from "./profile";
+
 export type profileListType = "movie" | "tv";
+
+export type ContextValue = {
+  toggleTheme: () => void;
+  appTheme: Theme;
+  mode: string;
+  user: Profile;
+  setUser: (value: Profile) => void;
+  showListModal: boolean;
+  toggleListModal: () => void;
+};
