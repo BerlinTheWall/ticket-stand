2cd586840bef46025cbd56a60118f1e91579ed89
Hooman Shahidi <hooman.ksh@gmail.com>
2024-02-25 15:04:06 +0330

    feat: add APIs to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,47 @@ import Box from "@mui/material/Box";
 import MainLayout from "@/layout/main-layout";
 import MovieSwiperXl from "@/components/swiper-slides/movie-swiper-xl";
 import { GetServerSideProps } from "next";
-import { getPopularMovies } from "@/api/movies";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getTrendingMovies,
+} from "@/api/movies";
 import { Movie } from "@/types/movie";
 import MovieSwiperMd from "@/components/swiper-slides/movie-swiper-md";
 import SwiperBanner from "@/components/swiper-slides/swiper-banner";
 import MovieSwiperSm from "@/components/swiper-slides/movie-swiper-sm";
 import MovieSwiperFeatured from "@/components/swiper-slides/movie-swiper-featured";
 import SearchBar from "@/components/search-bar";
-import { getPopularTVSeries } from "@/api/tv-series";
+import {
+  getPopularTVSeries,
+  getTopRatedTVSeries,
+  getTrendingTVSeries,
+} from "@/api/tv-series";
 import { TVSeries } from "@/types/tv-series";
+import {
+  POPULAR_MOVIES_PAGE,
+  POPULAR_TVSERIES_PAGE,
+  TOP_RATED_MOVIES_PAGE,
+  TOP_RATED_TVSERIES_PAGE,
+  TRENDING_TVSERIES_PAGE,
+} from "@/constants/urls";
 
 interface Props {
   movies: Movie[];
   tvSeries: TVSeries[];
+  trendingMovies: Movie[];
+  trendingTVSeries: TVSeries[];
+  topRatedMovies: Movie[];
+  topRatedTVSeries: TVSeries[];
 }
-export default function Home({ movies, tvSeries }: Props) {
+export default function Home({
+  movies,
+  tvSeries,
+  trendingMovies,
+  trendingTVSeries,
+  topRatedMovies,
+  topRatedTVSeries,
+}: Props) {
   return (
     <MainLayout>
       <Box
@@ -26,24 +52,52 @@ export default function Home({ movies, tvSeries }: Props) {
       >
         <SwiperBanner movies={movies} />
         <SearchBar />
-        <MovieSwiperXl title="Just Release" movies={movies} />
-        <MovieSwiperMd title="TV Series" items={tvSeries} />
-
-        <MovieSwiperSm title="Movies" movies={movies} />
-        <MovieSwiperFeatured movies={movies} />
-        <MovieSwiperMd title="Movies" items={movies} />
+        <MovieSwiperXl
+          title="Just Release"
+          movies={movies}
+          href={POPULAR_MOVIES_PAGE}
+        />
+        <MovieSwiperMd
+          title="TV Series"
+          items={tvSeries}
+          href={POPULAR_TVSERIES_PAGE}
+        />
+        <MovieSwiperSm
+          title="Top Rated Movies"
+          items={topRatedMovies}
+          href={TOP_RATED_MOVIES_PAGE}
+        />
+        <MovieSwiperFeatured movies={trendingMovies} />
+        <MovieSwiperSm
+          title="Top Rated TV Series"
+          items={topRatedTVSeries}
+          href={TOP_RATED_TVSERIES_PAGE}
+        />
+        <MovieSwiperMd
+          title="Trending TV Series"
+          items={trendingTVSeries}
+          href={TRENDING_TVSERIES_PAGE}
+        />
       </Box>
     </MainLayout>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const { data: movies } = await getPopularMovies({ page: 1 });
-  const { data: tvSeries } = await getPopularTVSeries({ page: 1 });
+  const movies = await getPopularMovies({ page: 1 });
+  const tvSeries = await getPopularTVSeries({ page: 1 });
+  const trendingMovies = await getTrendingMovies({ page: 1 });
+  const trendingTVSeries = await getTrendingTVSeries({ page: 1 });
+  const topRatedMovies = await getTopRatedMovies({ page: 1 });
+  const topRatedTVSeries = await getTopRatedTVSeries({ page: 1 });
   return {
     props: {
       movies: movies.results,
       tvSeries: tvSeries.results,
+      trendingMovies: trendingMovies.results,
+      trendingTVSeries: trendingTVSeries.results,
+      topRatedMovies: topRatedMovies.results,
+      topRatedTVSeries: topRatedTVSeries.results,
     },
   };
 };
