f85835466531753fb52e8f738a151da38e045db4
Hooman Shahidi <hooman.ksh@gmail.com>
2024-02-25 14:56:53 +0330

    chore: add constants

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,9 +1,21 @@
 const list = "list";
 const tvSeries = "episodes";
+const topTVSeries = "topTVSeries";
+const trendingTVSeries = "trendingTVSeries";
+const popularTVSeries = "popularTVSeries";
+const topMovies = "topMovies";
+const trendingMovies = "trendingMovies";
+const popularMovies = "popularMovies";
 
 const queryKeys = {
   list,
   tvSeries,
+  topTVSeries,
+  trendingTVSeries,
+  popularTVSeries,
+  topMovies,
+  trendingMovies,
+  popularMovies,
 };
 
 export default queryKeys;
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -5,3 +5,12 @@ export const SINGLE_MOVIE_PAGE = "/movie";
 export const SINGLE_TVSERIES_PAGE = "/tv-series";
 export const PROFILE_PAGE = "/profile";
 export const SINGLE_LIST_PAGE = "/list";
+export const ABOUT_US_PAGE = "/about-us";
+export const TRENDING_MOVIES_PAGE = "/movies/trending";
+export const POPULAR_MOVIES_PAGE = "/movies/popular";
+export const TOP_RATED_MOVIES_PAGE = "/movies/top-rated";
+export const TRENDING_TVSERIES_PAGE = "/tv-series/trending";
+export const POPULAR_TVSERIES_PAGE = "/tv-series/popular";
+export const TOP_RATED_TVSERIES_PAGE = "/tv-series/top-rated";
+
+export const SIGN_UP_PAGE = "https://www.themoviedb.org/signup";
