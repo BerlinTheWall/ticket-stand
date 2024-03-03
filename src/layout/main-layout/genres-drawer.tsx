c27f9ca8dc39d420f8abdd4b523f7b108e9201d5
farnoodlotfali <farnoodlotfali@gmail.com>
2024-03-03 15:46:14 +0330

    fix hover transition

--- src/layout/main-layout/genres-drawer.tsx
+++ src/layout/main-layout/genres-drawer.tsx
@@ -10,7 +10,6 @@ import {
   Box,
   Button,
   Divider,
-  Grid,
   Stack,
   Tooltip,
   Typography,
@@ -22,7 +21,7 @@ import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 
-export const DISCOVERITEMS= [
+export const DISCOVERITEMS = [
   {
     href: TOP_RATED_MOVIES_PAGE,
     name: "Top Rated Movies",
@@ -91,6 +90,51 @@ export const DISCOVER_TVSERIES_MENU = [
   },
 ];
 
+const ListGroup: React.FC<{
+  list: typeof DISCOVER_TVSERIES_MENU;
+  title: string;
+}> = ({ list, title }) => {
+  return (
+    <Stack>
+      <Typography
+        sx={{
+          textAlign: "center",
+          color: "primary.dark",
+          fontSize: 22,
+          fontWeight: "bold",
+        }}
+      >
+        {title}
+      </Typography>
+      <Stack spacing={2} py={2}>
+        {list.map((item) => {
+          return (
+            <Link key={item.name} href={item.href}>
+              <Stack direction="row" alignItems="center" spacing={2}>
+                {item.icon}
+                <Typography
+                  component="h3"
+                  sx={{
+                    fontWeight: 700,
+                    cursor: "pointer",
+                    transition: "all 0.2s ease-in-out",
+                    ":hover": {
+                      transform: "translateX(8px)",
+                      color: "primary.main",
+                    },
+                  }}
+                >
+                  {item.name}
+                </Typography>
+              </Stack>
+            </Link>
+          );
+        })}
+      </Stack>
+    </Stack>
+  );
+};
+
 const GenresDrawer: React.FC = () => {
   const theme = useTheme();
   const [isHover, setIsHover] = useState(false);
@@ -103,7 +147,7 @@ const GenresDrawer: React.FC = () => {
       <Tooltip
         title={
           <Box>
-    {/* <Grid container spacing={0}> */}
+            {/* <Grid container spacing={0}> */}
             {/* {MovieGenre.map((genre) => {
                 return (
                   <Grid item md={4} key={genre.id} sx={{ px: 2, my: 1.2 }}>
@@ -189,93 +233,10 @@ const GenresDrawer: React.FC = () => {
               })}
             </Grid> */}
             <Stack direction={"row"} justifyContent={"space-around"}>
-              <Stack direction={"column"}>
-                <Typography
-                  sx={{
-                    textAlign: "center",
-                    color: "primary.dark",
-                    fontSize: 22,
-                    fontWeight: "bold",
-                  }}
-                >
-                  Movies
-                </Typography>
-                {DISCOVER_MOVIES_MENU.map((item) => {
-                  return (
-                    <Grid item md={6} key={item.name} sx={{ px: 1, my: 1.2 }}>
-                      <Link href={item.href}>
-                        <Stack direction={"row"} alignItems={"center"} gap={1}>
-                          {item.icon}
-                          <Typography
-                            fontWeight={700}
-                            component="h3"
-                            sx={{
-                              pl: 1,
-                              cursor: "pointer",
-                              // borderLeft: `2px solid ${theme.palette.primary.main}`,
-                              transition: "padding-left 0.2s ease-in-out",
-                              ":hover": {
-                                pl: 1.5,
-                                color: theme.palette.primary.main,
-                              },
-                            }}
-                          >
-                            {item.name}
-                          </Typography>
-                        </Stack>
-                      </Link>
-                    </Grid>
-                  );
-                })}
-              </Stack>
+              <ListGroup list={DISCOVER_MOVIES_MENU} title="Movies" />
               <Divider orientation="vertical" flexItem />
-              <Stack direction={"column"}>
-                <Typography
-                  sx={{
-                    textAlign: "center",
-                    color: "primary.dark",
-                    fontSize: 22,
-                    fontWeight: "bold",
-                  }}
-                >
-                  TV Series
-                </Typography>
-                {DISCOVER_TVSERIES_MENU.map((item) => {
-                  return (
-                    <Grid item md={6} key={item.name} sx={{ px: 1, my: 1.2 }}>
-                      <Link href={item.href}>
-                        <Stack direction={"row"} alignItems={"center"} gap={1}>
-                          {item.icon}
-                          <Typography
-                            fontWeight={700}
-                            component="h3"
-                            sx={{
-                              pl: 1,
-                              cursor: "pointer",
-                              // borderLeft: `2px solid ${theme.palette.primary.main}`,
-                              transition: "padding-left 0.2s ease-in-out",
-                              ":hover": {
-                                pl: 1.5,
-                                color: theme.palette.primary.main,
-                              },
-                            }}
-                          >
-                            {item.name}
-                          </Typography>
-                        </Stack>
-                      </Link>
-                    </Grid>
-                  );
-                })}
-              </Stack>
+              <ListGroup list={DISCOVER_TVSERIES_MENU} title="TV Series" />
             </Stack>
-            <Stack
-              spacing={2}
-              component="ul"
-              sx={{
-                listStyle: "none",
-              }}
-            ></Stack>
           </Box>
         }
         placement="bottom-start"
